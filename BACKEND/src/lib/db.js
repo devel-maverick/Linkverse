80d5d1164d11d348381c34afb87bc74681a67009
devel-maverick <codemaverick00@gmail.com>
2025-11-12 00:34:38 +0530

    connected-postgresql

--- BACKEND/src/lib/db.js
+++ BACKEND/src/lib/db.js
@@ -1,14 +1,15 @@
 import { PrismaClient } from '@prisma/client';
+
 const prisma = new PrismaClient();
+
 export const connectDB = async () => {
   try {
     await prisma.$connect();
-    console.log("MySQL CONNECTED ✅");
+    console.log("PostgreSQL (Supabase) CONNECTED ✅");
   } catch (err) {
-    console.error('Database connection error:', err.message || err);
-    throw err;
+    console.error("Database connection error:", err.message || err);
+    process.exit(1); 
   }
 };
-
 
 export default prisma;
