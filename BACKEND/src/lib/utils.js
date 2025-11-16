124c89bf0766ca8fa8c692e24e963fce3feaeff1
devel-maverick <codemaverick00@gmail.com>
2025-11-16 15:11:20 +0530

    sidebar completed

--- BACKEND/src/lib/utils.js
+++ BACKEND/src/lib/utils.js
@@ -12,8 +12,8 @@ export const generateToken=(userId,res)=>{
     res.cookie('jwt',token,{
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: ENV.NODE_ENV === "development" ? "none" : "strict",
-    secure: ENV.NODE_ENV === "development" ? false : true, 
+    sameSite:ENV.NODE_ENV=="development" ?"None":"Strict",
+    secure: true ,
     path: "/"
     })
     return token;
