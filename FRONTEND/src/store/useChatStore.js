a6e8f583a833dfbcf73b77e4bc000e4e4d46dd16
devel-maverick <codemaverick00@gmail.com>
2025-11-17 01:21:32 +0530

    message input implemented

--- FRONTEND/src/store/useChatStore.js
+++ FRONTEND/src/store/useChatStore.js
@@ -1,6 +1,7 @@
 import {create} from 'zustand'
 import { axiosInstance } from '../lib/axios';
 import toast from 'react-hot-toast';
+import { useAuthStore } from './useAuthStore';
 export const useChatStore=create((set,get)=>({
     allContacts:[],
     chats:[],
@@ -72,6 +73,30 @@ export const useChatStore=create((set,get)=>({
 
         }
         
+    },
+    sendMessage:async(messageData)=>{
+        const {selectedUser,messages}=get()
+        const {authUser} =useAuthStore.getState()
+        const tempId=`temp-${Date.now()}`
+        const optimisticMessage={
+            id:tempId,
+            senderId:authUser.id,
+            recieverId:selectedUser.id,
+            text:messageData.text,
+            image:messageData.image,
+            createdAt:new Date().toISOString(),
+            isOptimistic:true
+
+        };
+        set({messages:[...messages,optimisticMessage]})
+        try{
+            const res=await axiosInstance.post(`/messages/send/${selectedUser.id}`,messageData)
+            set({messages:messages.concat(res.data)})
+        }catch(err){
+            set({messages:messages})
+            toast.error(error.response?.data?.message || "something went wrong")
+
+        }
     }
 
 }))
