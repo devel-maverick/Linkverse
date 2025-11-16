a9685c4cd7ea511d11fc6d8b4dd4cf00ac6bded9
devel-maverick <codemaverick00@gmail.com>
2025-11-16 15:27:44 +0530

    issue fixed

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect } from 'react'
 import { Navigate, Route, Routes } from 'react-router'
 import LoginPage from './pages/LoginPage'
-import SignUpPage from './pages/signUpPage'
+import SignUpPage from './pages/SignUpPage'
 import ChatPage from './pages/ChatPage'
 import { useAuthStore } from './store/useAuthStore'
 import PageLoader from './components/PageLoader'
